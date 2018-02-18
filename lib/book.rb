184550dd6dd5e43854eaea674393ad171cbfa292
Marin Cabac <marin.cabac@gmail.com>
2018-02-18 21:50:00 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,10 +8,8 @@ attr_accessor :author, :page_count, :genre
     @title=title
   end
 
-  def title
-    @title
-  end
-
+   attr_reader :title 
+   
   def turn_page
 
 puts "Flipping the page...wow, you read fast!"
