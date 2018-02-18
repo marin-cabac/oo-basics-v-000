b608ce444f2a36beff3442413f6fef58305601fc
Marin Cabac <marin.cabac@gmail.com>
2018-02-18 21:38:02 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,20 +3,27 @@
 class Book
 
   def initialize(title)
-    @title=title 
+    @title=title
   end
 
   def title
     @title
   end
-  
+
   def author=(author)
     @author=author
-  end 
+  end
 
   def author
     @author
   end 
 
-end
+  def page_count(num)
+    @page_count=num
+  end 
 
+  def page_count
+    @page_count 
+  end 
+
+end
