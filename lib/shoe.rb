e4e1161a129538b58e5ce3fb8bbd7880eb67f2b5
Marin Cabac <marin.cabac@gmail.com>
2018-02-18 22:04:17 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,13 +13,4 @@ class Shoe
 end
 
 
-describe '#cobble' do
-  it 'says that the shoe has been repaired' do
-    expect($stdout).to receive(:puts).with("Your shoe is as good as new!")
-    shoe.cobble
-  end
-
-  it 'makes the shoe\'s condition new' do
-    shoe.condition = "old"
-    shoe.cobble
-    expect(shoe.condition).to eq("new")
+ 
