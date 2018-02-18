9958e39028da47daa5d3a5e1a4615e4a26ecf2d0
Marin Cabac <marin.cabac@gmail.com>
2018-02-18 22:07:23 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,6 +8,6 @@ class Shoe
   attr_reader :brand
 
  def cobble
-  @condition.eql?("new") ? (puts "Your shoe is as good as new!") : (@condition="new")
+  @condition.eql?("new") ? (puts "Your shoe is as good as new!") : (@condition="old")
  end
 end
